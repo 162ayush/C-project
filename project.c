acbda7d8637dff65b6951230b01ff69550a40a58
ayush shivahre <114332353+AyushRai16@users.noreply.github.com>
2023-02-24 10:56:53 +0530

    Add files via upload

---
+++ project.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+// GAME TO GUESS A CORRECT NUMBER
+#include<stdlib.h>
+#include<time.h>
+int main(){
+srand(time(0));
+int number= rand()%100; 
+int guess;
+int attempt =1;
+for(int i=0;i<10;i++){
+do {
+    printf("guess the number between 1 to 100\n");
+    scanf("%d",&guess);
+    if (guess>number){
+        printf("please guess lower number\n");
+    }
+    else if(number>guess){
+        printf("please guess higher number\n");
+    }
+    else{
+        if (attempt==1){
+            printf("wohh! its great\n you had done in 1 attempt\n");
+            }
+            
+       else{
+        printf("congratulation  correct number after %d attempts :\n",attempt);
+       }
+        
+    }
+    attempt++;
+}
+
+while(number!=guess);
+ 
+printf("\nWELCOME TO NEW GAME\n");
+if(i==9){
+    printf("OHH! YOU HAD PLAYED ENOUGH TIMES");
+}
+}
+
+return 0;
+}
